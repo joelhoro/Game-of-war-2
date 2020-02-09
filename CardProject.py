5e29bcb4917d85713cce559664c956c161b5aa5f
Joel Horowitz <joelhoro@gmail.com>
2020-02-09 08:44:27 -0500

    Pycharm cleanup

--- CardProject.py
+++ CardProject.py
@@ -1,5 +1,6 @@
 import random
 
+
 def setUp():
     print('/////-----Welcome To WAR!-----\\\\\\\\\\')
     player1 = player(50, input("What is your name?  "))
@@ -10,26 +11,28 @@ def setUp():
 
     setBet(player1)
 
+
 def setBet(player):
     loop = True
     while loop:
         try:
             bet = int(input('How much would you like to bet %s?' % player.name))
             if bet > player.balance:
-                print ("You don't have enough money!")
+                print("You don't have enough money!")
             else:
                 loop = False
         except:
-            print ('You must enter a number.')
+            print('You must enter a number.')
     player.bet = bet
     makeBet(player)
+
 
 def makeBet(player):
     player.card = pickNumber(10)
 
-    print("%s got the %s of %s" %(player.name, player.card, pickSuite()))
+    print("%s got the %s of %s" % (player.name, player.card, pickSuite()))
     computerCard = pickNumber(10)
-    print("The computer got the %s of %s" %(computerCard, pickSuite()))
+    print("The computer got the %s of %s" % (computerCard, pickSuite()))
     if computerCard >= player.card:
         print("That means that you lost!")
         player.remove(player.bet)
@@ -51,6 +54,7 @@ def newGame(player):
         print("Bye Bye %s, enjoy the money and don't forget to gamble in moderation!" % player.name)
         exit()
 
+
 def pickSuite():
     number = pickNumber(4)
     if number == 1:
@@ -65,11 +69,14 @@ def pickSuite():
 
 
 def pickNumber(n):
-     x = random.randint(1,n)
-     return x
+    x = random.randint(1, n)
+    return x
+
 
 def printInstructions():
-    print("You and the computer will pick random cards from 1 to 10 and whoever gets the highest card wins, if it is a draw, the computer wins: the house always wins ;)")
+    print(
+        "You and the computer will pick random cards from 1 to 10 and whoever gets the highest card wins, if it is a draw, the computer wins: the house always wins ;)")
+
 
 def getAnswer(message):
     loop = True
@@ -87,16 +94,16 @@ def getAnswer(message):
 
 
 class player():
-    def __init__(self,balance, name):
+    def __init__(self, balance, name):
         self.balance = balance
         self.name = name
         self.bet = 0
         self.card = 0
 
-    def remove(self,amount):
+    def remove(self, amount):
         self.balance -= amount
 
-    def add(self,amount):
+    def add(self, amount):
         self.balance += amount
 
     def __repr__(self):
